62280a1fab858bf6af5c503e906508e2162e2202
Brito201 <cristian201_brito@hotmail.com>
2021-12-12 21:47:31 -0300

    primer commit

---
+++ Test.inicio.spec.js
@@ -0,0 +1,19 @@
+/// <reference types = "Cypress" />
+
+beforeEach(() =>{
+    cy.visit('Direccion web')
+} )
+
+describe('Pruebas de X', function(){
+    it('validacion de inicio', function(){
+
+        cy.visit('Direccion web')
+        cy.visit('holamundo')
+
+
+
+
+
+
+    })
+})
